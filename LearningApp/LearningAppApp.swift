2358c725b40e45dba32bbbf3f1cfe9e871191787
idan cohen <idan000189@gmail.com>
2022-04-25 21:15:56 +0300

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by idan Cohen on 25/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
